32e4ac913d2fb77987976b6eeecfdb3aeea0c5ba
Wismal <albertowisma@gmail.com>
2024-05-07 16:21:08 +0200

    Finalizado

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-let activeInformationAfter = document.querySelector(".activeInformationAfter")
+
+let popUp = document.querySelector(".information-popup")
+let shareButton = document.getElementById("shareButton")
+
+shareButton.addEventListener("click", () => popUp.classList.toggle("hidden"))
+
